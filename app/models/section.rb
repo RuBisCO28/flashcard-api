81820afb2bb3da4dd58ec3e99bf7ed2a89c6ad55
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2022-04-10 17:32:26 +0900

    fix migration

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,6 +6,15 @@
 #  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  book_id    :bigint
+#
+# Indexes
+#
+#  index_sections_on_book_id  (book_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (book_id => books.id)
 #
 class Section < ApplicationRecord
   has_many :contents
--- db/migrate/20220410083000_create_sections.rb
+++ db/migrate/20220410083000_create_sections.rb
@@ -3,6 +3,7 @@ class CreateSections < ActiveRecord::Migration[7.0]
     create_table :sections do |t|
 
       t.string :name, null: false
+      t.references :book, foreign_key: true
       t.timestamps
     end
   end
